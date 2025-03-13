c39288c5672183fcec903e328af6a43b5a825ba3
willymariino <wg.mariino@gmail.com>
2025-03-13 17:37:12 +0100

    added cicle and innerhtml

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
 // indico dove voglio andare a posizionare le email
 const emailContainer = document.getElementById("email-container")
 const emailList = document.getElementById("email-list")
+
+const emailCount = 10
+
+for (let i = 0; i < emailCount; i++) {
+    // chiamo  l'API che voglio usare
+    axios.get(endpoint)
+        .then(response) => {
+
+        // creo elementi html 
+        emailList.innerHTML = `<li>${email}<li>`
+
+
+
+    }
+    .catch (error => {
+        console.log("errore", error)
+    })
+
+
+
+}
