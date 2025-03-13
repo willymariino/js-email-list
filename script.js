b55e07ac35ecfefd6edf7806a296b2536b1677ca
willymariino <wg.mariino@gmail.com>
2025-03-13 18:15:43 +0100

    fixed parentesis

--- script.js
+++ script.js
@@ -12,18 +12,20 @@ const emailCount = 10
 
 for (let i = 0; i < emailCount; i++) {
     // chiamo  l'API che voglio usare
-    axios.get(endpoint).then(response) => {
+    axios.get(endpoint)
+        .then(response => {
 
-        const email = response.data.posts;
+            const email = response.data.response;
+            console.log(response)
 
-        // creo elementi html 
-        emailList.innerHTML = `<li>${email}<li>`
+            // creo elementi html 
+            emailList.innerHTML += `<li>${email}<li>`
 
 
 
-    } .catch (error => {
-        console.log("errore", error)
-    })
+        }).catch(error => {
+            console.log("errore", error)
+        })
 
 
 
