a53d260d12656fcee9ead000aba155a254c354d6
willymariino <wg.mariino@gmail.com>
2025-03-13 16:37:29 +0100

    email list structure added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // generatore di email casuali
 
+
+// inserisco il server da cui vado a prendere le email
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
 
+// indico dove voglio andare a posizionare le email
 const emailContainer = document.getElementById("email-container")
+const emailList = document.getElementById("email-list")
